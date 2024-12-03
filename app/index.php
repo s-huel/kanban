9213b6a943962c1d7342c2354136effbc6e00ed4
Felix <felix.huel6@gmail.com>
2024-12-03 10:51:05 +0100

    update Frontend for the page, only a base. Database is next, then the connection and the logic:

---
+++ app/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Todo-List</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+  <div class="board">
+    <div class="column">
+      <h2>Todo</h2>
+      <div class="line"></div>
+    </div>
+
+    <div class="column">
+      <h2>In Progress</h2>
+      <div class="line"></div>
+    </div>
+    
+    <div class="column">
+      <h2>Done</h2>
+      <div class="line"></div>
+    </div>
+  </div>
+
+</body>
+</html>
