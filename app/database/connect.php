8fda7f4227ab2e879a9c156a93c5beae24ad8f7c
Felix <felix.huel6@gmail.com>
2025-01-30 14:37:07 +0100

    update Database toevoegingen en aanpassingen connect

--- app/database/connect.php
+++ app/database/connect.php
@@ -11,5 +11,3 @@ try {
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-
-?>
--- app/database/import.sql
+++ app/database/import.sql
@@ -15,3 +15,9 @@ CREATE TABLE item (
 );
 
 INSERT INTO lane (title) VALUES ('Todo'), ('In Progress'), ('Done');
+
+INSERT INTO item (lane_id, title) VALUES 
+(1, 'Maak een GIT repository aan'),
+(1, 'Setup database met twee tabellen'),
+(2, 'Begin met het schrijven van PHP-code'),
+(3, 'Lees de projectvereisten');
